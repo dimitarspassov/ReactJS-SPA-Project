bbbf9329d10df6a9692e5146acd95f052d469b6c
inaabadjieva <ina_abadjieva@yahoo.com>
2016-12-02 23:13:03 +0200

    Register update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
-import LoginForm from './LoginForm';
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                <span>Login Page</span>
-                <LoginForm />
-            </div>
+            <div className="container">
+                   <div className="box">
+                      <hr/>
+                      <h2 className="intro-text text-center">
+                          <strong>Login form</strong>
+                      </h2>
+                      <hr/>
+                      <form className="form" role="form"> 
+                          <div className="row">
+                            <div className="form-group col-xs-offset-4 col-xs-4">
+                               <label>Username</label>
+                               <input type="text" className="form-control"/>
+                            </div>
+                        </div>
+                         <div className="row">
+                            <div className="form-group col-xs-offset-4 col-xs-4">
+                               <label>Password</label>
+                               <input type="tel" className="form-control"/>
+                            </div>
+                          </div>
+                         <div className="row">
+                            <div className="form-group col-lg-12">
+                               <input type="hidden" name="save" value="contact"/>
+                               <button type="submit" className="btn btn-default">Submit</button>
+                            </div>
+                          </div>
+                      </form>
+                  </div>
+          </div>
         );
     }
 }
