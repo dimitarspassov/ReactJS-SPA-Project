10a95a3f152bf41b49b8c897c84391bf33f23bc5
Dimitar Spassov <dimitar.spassov7@gmail.com>
2016-12-06 17:11:40 +0200

    Added sorting function for Events page

--- src/components/Events.js
+++ src/components/Events.js
@@ -20,10 +20,12 @@ class Events extends React.Component {
         this.state = {
             events: [],
             activePage: 1,
-            eventRows: []
+            eventRows: [],
+            sorted: false
         };
         this.onLoadSuccess = this.onLoadSuccess.bind(this);
         this.handlePageChange = this.handlePageChange.bind(this);
+        this.handleSortingChange = this.handleSortingChange.bind(this);
     }
 
     componentDidMount() {
@@ -31,33 +33,81 @@ class Events extends React.Component {
     }
 
     onLoadSuccess(response) {
-        this.setState({
-            events: response,
-            eventRows: response.chunk_inefficient(3),
-        })
+
+        if (this.state.sorted) {
+            this.setState({
+                events: response,
+                eventRows: response.chunk_inefficient(3),
+            })
+        } else if (this.state.sorted === false) {
+            this.setState({
+                events: response.sort((a, b) => new Date(b._kmd.ect) - new Date(a._kmd.ect)),
+                eventRows: response.chunk_inefficient(3),
+            })
+        }
+
     }
 
     handlePageChange(pageNumber) {
 
         this.setState(
-            {activePage: pageNumber,});
+            {activePage: pageNumber});
     }
 
-    renderEvents(eventsToRender){
+    handleSortingChange(selector) {
 
-        return eventsToRender.map(event =>
-            <div key={event._id} id={event._id} className="col-md-4 portfolio-item">
-                <Link to={"/details/" + event._id}>
-                    <img className="img-responsive img-thumbnail" src={event.image} alt=""/>
-                </Link>
-                <h3>{event.title}
-                    <br/>
-                    <small>{event.date}</small>
-                </h3>
-                <p>{(event.description).slice(0, 75)}</p>
-                <Link className="btn btn-default btn-lg"
-                      to={"/details/" + event._id}>ReadMore</Link>
-            </div>)
+        if (selector === "added") {
+
+            this.setState({
+                events: this.state.events.sort((a, b) => new Date(b._kmd.ect) - new Date(a._kmd.ect)),
+                eventsRows: this.state.events.sort((a, b) => new Date(b._kmd.ect) - new Date(a._kmd.ect)).chunk_inefficient(3),
+                activePage: 1,
+                sorted: true
+            });
+
+            this.onLoadSuccess(this.state.events)
+        } else if (selector === "edited") {
+
+            this.setState({
+                events: this.state.events.sort((a, b) => new Date(b._kmd.lmt) - new Date(a._kmd.lmt)),
+                eventsRows: this.state.events.sort((a, b) => new Date(b._kmd.lmt) - new Date(a._kmd.lmt)).chunk_inefficient(3),
+                activePage: 1,
+                sorted: true
+            });
+
+            this.onLoadSuccess(this.state.events)
+        } else if (selector === "soonest") {
+
+            this.setState({
+                events: this.state.events.sort((a, b) => new Date(a.date) - new Date(b.date)),
+                eventsRows: this.state.events.sort((a, b) => new Date(a.date) - new Date(b.date)).chunk_inefficient(3),
+                activePage: 1,
+                sorted: true
+            });
+
+            this.onLoadSuccess(this.state.events);
+        }
+    }
+
+
+    renderEvents(eventsToRender) {
+
+        if (eventsToRender !== undefined) {
+            return eventsToRender.map(event =>
+                <div key={event._id} id={event._id} className="col-md-4 portfolio-item">
+                    <Link to={"/details/" + event._id}>
+                        <img className="img-responsive img-thumbnail" src={event.image} alt=""/>
+                    </Link>
+                    <h3>{event.title}
+                        <br/>
+                        <small>{event.date}</small>
+                    </h3>
+                    <p>{(event.description).slice(0, 75)}</p>
+                    <Link className="btn btn-default btn-lg"
+                          to={"/details/" + event._id}>ReadMore</Link>
+                </div>)
+        }
+
     }
 
     render() {
@@ -69,23 +119,40 @@ class Events extends React.Component {
                         <strong>Events</strong>
                     </h2>
                     <hr/>
+
                     <div className="row">
-                    {this.state.eventRows[Number(this.state.activePage)-1]!==undefined ?
+                        <h2 className="intro-text text-center">Sort by:</h2>
+                        <div className="intro-text text-center">
+                            <button className="btn btn-default" onClick={() => this.handleSortingChange('added')}>Date
+                                added
+                            </button>
+                            <button className="btn btn-default" onClick={() => this.handleSortingChange('edited')}>Last
+                                edited
+                            </button>
+                            <button className="btn btn-default" onClick={() => this.handleSortingChange('soonest')}>
+                                Soonest first
+                            </button>
+                        </div>
+                        <br></br>
+                    </div>
 
-                            this.renderEvents(this.state.eventRows[Number(this.state.activePage)-1])
+                    <div className="row">
+                        {this.state.eventRows[Number(this.state.activePage) - 1] !== undefined ?
 
-                        :
-                        console.log()
-                    }
+                            this.renderEvents(this.state.eventRows[Number(this.state.activePage) - 1])
+
+                            :
+                            console.log()
+                        }
                     </div>
                     <div className="text-center">
-                    <Pagination
-                        activePage={this.state.activePage}
-                        itemsCountPerPage={3}
-                        totalItemsCount={this.state.events.length}
-                        pageRangeDisplayed={5}
-                        onChange={this.handlePageChange}
-                    />
+                        <Pagination
+                            activePage={this.state.activePage}
+                            itemsCountPerPage={3}
+                            totalItemsCount={this.state.events.length}
+                            pageRangeDisplayed={5}
+                            onChange={this.handlePageChange}
+                        />
                     </div>
 
                 </div>
